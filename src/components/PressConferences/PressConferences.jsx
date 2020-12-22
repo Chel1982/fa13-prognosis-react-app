644fcdeb22128516ff1ea016ce6404317b77f3cb
chel1982 <chel@activist.com>
2020-12-22 17:29:47 +0200

    many changes

--- src/components/PressConferences/PressConferences.jsx
+++ src/components/PressConferences/PressConferences.jsx
@@ -1,80 +1,248 @@
 import React from "react";
+import PressConferencesCss from "./PressConferences.module.css";
 
 const PressConferences = () => {
     return (
         <div>
             <div>
-                <h3>
-                    Чемпионат Австралии, тур-4
-                </h3>
+                <div className={PressConferencesCss.match}>
+                    <div>
+                        <h4>
+                            Чемпионат Австралии, тур-4,
+                            матч: Мельбурн Виктори(1) - Алтона Мэджик(0)
+                        </h4>
+                    </div>
+                    <div>
+                        <a href="http://old.fa13.info/video/match.php?video=8698" target="_blank">
+                            <div className={PressConferencesCss.videoSource}></div>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://repository.fa13.info/site/pub/html/rAs/1721/C02-V32.htm" target="_blank">
+                            <div className={PressConferencesCss.textSource}></div>
+                        </a>
+                    </div>
+                </div>
                 <div>
-                    <h4>
-                        Матч: Мельбурн Виктори(1) - Алтона Мэджик(0) Видео Текст
-                    </h4>
-                    <div>
-                        <button>
-                            Комментировать
-                        </button>
-                        <button>
-                            Смотреть комментарии
-                        </button>
-                    </div>
-                    <h4>
-                        <b>Менеджер команды "Алтона Мэджик"</b>
-                    </h4>
-                    <p>
-                        <time>23:19 23.09.2020</time>
-                    </p>
-                    <p>[23.09.2020 23:19:17] Команда провела отличный гостевой матч автосоставом с сильным
-                        соперником, хотя для нас он и получился &quot;на толстенького&quot;. Моих решений не было,
-                        потому комментировать больше нечего</p>
-                    <div>
-                        <button>
-                            Комментировать
-                        </button>
-                        <button>
-                            Смотреть комментарии
-                        </button>
-                    </div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
+                </div>
+                <h4>
+                    <b>Менеджер команды "Алтона Мэджик"</b>
+                </h4>
+                <p>
+                    <time>23:19 23.09.2020</time>
+                </p>
+                <p>[23.09.2020 23:19:17] Команда провела отличный гостевой матч автосоставом с сильным
+                    соперником, хотя для нас он и получился &quot;на толстенького&quot;. Моих решений не было,
+                    потому комментировать больше нечего</p>
+                <div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
                 </div>
             </div>
             <hr/>
             <div>
-                <h3>
-                    Чемпионат Австралии, тур-4
-                </h3>
+                <div className={PressConferencesCss.match}>
+                    <div>
+                        <h4>
+                            Чемпионат Австралии, тур-4,
+                            матч: Мельбурн Виктори(1) - Алтона Мэджик(0)
+                        </h4>
+                    </div>
+                    <div>
+                        <a href="http://old.fa13.info/video/match.php?video=8698" target="_blank">
+                            <div className={PressConferencesCss.videoSource}></div>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://repository.fa13.info/site/pub/html/rAs/1721/C02-V32.htm" target="_blank">
+                            <div className={PressConferencesCss.textSource}></div>
+                        </a>
+                    </div>
+                </div>
                 <div>
-                    <h4>
-                        Матч: Мельбурн Виктори(1) - Алтона Мэджик(0) Видео Текст
-                    </h4>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
+                </div>
+                <h4>
+                    <b>Менеджер команды "Мельбурн Виктори"</b>
+                </h4>
+                <p>
+                    <time>23:06 16.09.2020</time>
+                </p>
+                <p>[16.09.2020 23:06:07] Очень &quot;на тоненького&quot; сегодня сыграли, даже преимущество в
+                    физе
+                    не помогло, все решил один момент во второй тайме, когда Купер убежал на рандеву с вратарем
+                    гостей, чудом вырвав победу. Надеюсь и верю, что тот резерв, что нам удалось сегодня
+                    сэкономить,
+                    позволит победить манкунианцев на их поле.</p>
+                <div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className={PressConferencesCss.match}>
                     <div>
-                        <button>
-                            Комментировать
-                        </button>
-                        <button>
-                            Смотреть комментарии
-                        </button>
+                        <h4>
+                            Чемпионат Австралии, тур-4,
+                            матч: Мельбурн Виктори(1) - Алтона Мэджик(0)
+                        </h4>
                     </div>
-                    <h4>
-                        <b>Менеджер команды "Мельбурн Виктори"</b>
-                    </h4>
-                    <p>
-                        <time>23:06 16.09.2020</time>
-                    </p>
-                    <p>[16.09.2020 23:06:07] Очень &quot;на тоненького&quot; сегодня сыграли, даже преимущество в
-                        физе
-                        не помогло, все решил один момент во второй тайме, когда Купер убежал на рандеву с вратарем
-                        гостей, чудом вырвав победу. Надеюсь и верю, что тот резерв, что нам удалось сегодня
-                        сэкономить,
-                        позволит победить манкунианцев на их поле.</p>
                     <div>
-                        <button>
-                            Комментировать
-                        </button>
-                        <button>
-                            Смотреть комментарии
-                        </button>
+                        <a href="http://old.fa13.info/video/match.php?video=8698" target="_blank">
+                            <div className={PressConferencesCss.videoSource}></div>
+                        </a>
                     </div>
+                    <div>
+                        <a href="http://repository.fa13.info/site/pub/html/rAs/1721/C02-V32.htm" target="_blank">
+                            <div className={PressConferencesCss.textSource}></div>
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
+                </div>
+                <h4>
+                    <b>Менеджер команды "Мельбурн Виктори"</b>
+                </h4>
+                <p>
+                    <time>23:06 16.09.2020</time>
+                </p>
+                <p>[16.09.2020 23:06:07] Очень &quot;на тоненького&quot; сегодня сыграли, даже преимущество в
+                    физе
+                    не помогло, все решил один момент во второй тайме, когда Купер убежал на рандеву с вратарем
+                    гостей, чудом вырвав победу. Надеюсь и верю, что тот резерв, что нам удалось сегодня
+                    сэкономить,
+                    позволит победить манкунианцев на их поле.</p>
+                <div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className={PressConferencesCss.match}>
+                    <div>
+                        <h4>
+                            Чемпионат Австралии, тур-4,
+                            матч: Мельбурн Виктори(1) - Алтона Мэджик(0)
+                        </h4>
+                    </div>
+                    <div>
+                        <a href="http://old.fa13.info/video/match.php?video=8698" target="_blank">
+                            <div className={PressConferencesCss.videoSource}></div>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://repository.fa13.info/site/pub/html/rAs/1721/C02-V32.htm" target="_blank">
+                            <div className={PressConferencesCss.textSource}></div>
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
+                </div>
+                <h4>
+                    <b>Менеджер команды "Мельбурн Виктори"</b>
+                </h4>
+                <p>
+                    <time>23:06 16.09.2020</time>
+                </p>
+                <p>[16.09.2020 23:06:07] Очень &quot;на тоненького&quot; сегодня сыграли, даже преимущество в
+                    физе
+                    не помогло, все решил один момент во второй тайме, когда Купер убежал на рандеву с вратарем
+                    гостей, чудом вырвав победу. Надеюсь и верю, что тот резерв, что нам удалось сегодня
+                    сэкономить,
+                    позволит победить манкунианцев на их поле.</p>
+                <div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className={PressConferencesCss.match}>
+                    <div>
+                        <h4>
+                            Чемпионат Австралии, тур-4,
+                            матч: Мельбурн Виктори(1) - Алтона Мэджик(0)
+                        </h4>
+                    </div>
+                    <div>
+                        <a href="http://old.fa13.info/video/match.php?video=8698" target="_blank">
+                            <div className={PressConferencesCss.videoSource}></div>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="http://repository.fa13.info/site/pub/html/rAs/1721/C02-V32.htm" target="_blank">
+                            <div className={PressConferencesCss.textSource}></div>
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
+                </div>
+                <h4>
+                    <b>Менеджер команды "Мельбурн Виктори"</b>
+                </h4>
+                <p>
+                    <time>23:06 16.09.2020</time>
+                </p>
+                <p>[16.09.2020 23:06:07] Очень &quot;на тоненького&quot; сегодня сыграли, даже преимущество в
+                    физе
+                    не помогло, все решил один момент во второй тайме, когда Купер убежал на рандеву с вратарем
+                    гостей, чудом вырвав победу. Надеюсь и верю, что тот резерв, что нам удалось сегодня
+                    сэкономить,
+                    позволит победить манкунианцев на их поле.</p>
+                <div>
+                    <button>
+                        Комментировать
+                    </button>
+                    <button>
+                        Смотреть комментарии
+                    </button>
                 </div>
             </div>
             <hr/>
