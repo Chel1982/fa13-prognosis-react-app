7f918a69c17662ec6079e4cf74d6dfaa1f14d765
chel1982 <chel@activist.com>
2020-12-30 16:48:45 +0200

    add rel value

--- src/components/Source/Source.jsx
+++ src/components/Source/Source.jsx
@@ -15,7 +15,7 @@ const Source = (props) => {
     }
     return (
         <div>
-            <a href={props.source} target="_blank">
+            <a href={props.source} target="_blank" rel="noopener noreferrer">
                 {res}
             </a>
         </div>
