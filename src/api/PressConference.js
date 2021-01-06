20b0938a21151a1f2f70e2ddfe99606f9d285b10
chel1982 <chel@activist.com>
2021-01-06 13:05:47 +0200

    add main url and custom url

--- src/api/PressConference.js
+++ src/api/PressConference.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 
+const MAIN_URL = 'http://api.fa13-prognosis.loc/api/v1/';
+
 export const getLastPressConference = async (countPressConf) => {
-    return await axios.get('http://api.fa13-prognosis.loc/api/v1/press-conferences/' + countPressConf)
+    return await axios.get(MAIN_URL + 'press-conferences/' + countPressConf)
         .then(response => response);
 }
 
 export const getLastPressConferencePagination = async (data, countPressConf) => {
-    return await axios.get('http://api.fa13-prognosis.loc/api/v1/press-conferences/'
+    return await axios.get(MAIN_URL + 'press-conferences/'
         + countPressConf + '?page=' + data.page)
         .then(response => response);
+}
+
+export const getTournamentList = async (status) => {
+    return await axios.get(MAIN_URL + 'tournament-list/' + status)
+        .then(response => response)
 }
