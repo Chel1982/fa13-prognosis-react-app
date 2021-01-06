14abb92aeaa3f6a2a67d1c83e5f55d7b043cb475
chel1982 <chel@activist.com>
2021-01-06 13:04:26 +0200

    add lastAllPressConferences and change name

--- src/components/PressConference/PressConferenceContainer.jsx
+++ src/components/PressConference/PressConferenceContainer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import {connect} from "react-redux";
 import PressConference from "./PressConference";
-import {getLastPressConferencesPaginationThunk, getLastPressConferencesThunk} from "../../redux/PressConferenceReducer";
+import {getLastAllPressConferencesPaginationThunk, getLastAllPressConferencesThunk} from "../../redux/PressConferenceReducer";
 import {Pagination} from 'react-laravel-paginex';
 import PaginationCss from "../Pagination/Pagination.module.css";
 
 class PressConferencesContainer extends React.Component {
 
     componentDidMount() {
-        this.props.getLastPressConferencesThunk();
+        this.props.getLastAllPressConferencesThunk();
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -19,18 +19,19 @@ class PressConferencesContainer extends React.Component {
     }
 
     render() {
-        if (this.props.pressConferenceReducer.data) {
+        if (this.props.pressConferenceReducer.lastAllPressConferences) {
             let result = [];
-            for (let key in this.props.pressConferenceReducer.data) {
+            for (let key in this.props.pressConferenceReducer.lastAllPressConferences.data) {
                 result.push(<PressConference
-                    key={this.props.pressConferenceReducer.data[key].id} {...this.props.pressConferenceReducer.data[key]} />)
+                    key={this.props.pressConferenceReducer.lastAllPressConferences.data[key].id}
+                    {...this.props.pressConferenceReducer.lastAllPressConferences.data[key]} />)
             }
             return (
                 <div>
                     {result.map(press => (press))}
                     <Pagination
-                        changePage={this.props.getLastPressConferencesPaginationThunk}
-                        data={this.props.pressConferenceReducer}
+                        changePage={this.props.getLastAllPressConferencesPaginationThunk}
+                        data={this.props.pressConferenceReducer.lastAllPressConferences}
                         options={this.options}
                         nextButtonText="Следующая"
                         prevButtonText="Предыдущая"
@@ -54,5 +55,5 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, {
-    getLastPressConferencesThunk, getLastPressConferencesPaginationThunk
+    getLastAllPressConferencesThunk, getLastAllPressConferencesPaginationThunk
 })(PressConferencesContainer);
