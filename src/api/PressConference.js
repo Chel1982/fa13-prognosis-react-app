627105fac0c7db5002f9bd0a0583fcdabc62c54c
chel1982 <chel@activist.com>
2021-01-26 10:33:02 +0200

    change Tournament List

--- src/api/PressConference.js
+++ src/api/PressConference.js
@@ -5,16 +5,9 @@ const MAIN_URL = 'http://api.fa13-prognosis.loc/api/v1/';
 export const getLastPressConference = async (data = '', countPressConf) => {
     if (data) {
         return await axios.get(MAIN_URL + 'press-conferences/'
-            + countPressConf + '?page=' + data.page)
-            .then(response => response);
+            + countPressConf + '?page=' + data.page);
     }
-    return await axios.get(MAIN_URL + 'press-conferences/' + countPressConf)
-        .then(response => response);
-}
-
-export const getTournamentList = async (status) => {
-    return await axios.get(MAIN_URL + 'tournament-list/' + status)
-        .then(response => response)
+    return await axios.get(MAIN_URL + 'press-conferences/' + countPressConf);
 }
 
 export const getForIdLastPressConferences = async (data = '', tournamentId, countPressConf) => {
@@ -25,13 +18,11 @@ export const getForIdLastPressConferences = async (data = '', tournamentId, coun
             + '/count/'
             + countPressConf
             + '?page='
-            + data.page)
-            .then(response => response);
+            + data.page);
     }
     return await axios.get(MAIN_URL
         + 'press-conferences/tournament_id/'
         + tournamentId
         + '/count/'
-        + countPressConf)
-        .then(response => response);
+        + countPressConf);
 }
--- src/redux/ReduxStore.js
+++ src/redux/ReduxStore.js
@@ -2,10 +2,14 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import pressConferenceReducer from "./PressConferenceReducer";
 import thunk from "redux-thunk";
 import tournamentListReducer from "./TournamentListReducer";
+import authReducer from "./AuthReducer";
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     pressConferenceReducer,
-    tournamentListReducer
+    tournamentListReducer,
+    authReducer,
+    form: formReducer
 });
 
 const store = createStore(reducers, applyMiddleware(thunk));
