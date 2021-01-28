885f14ba4a6d918e2b92d67c1556bd9cb420edff
chel1982 <chel@activist.com>
2021-01-28 14:43:22 +0200

    fix css style

--- src/components/PressConference/PressConference.jsx
+++ src/components/PressConference/PressConference.jsx
@@ -4,31 +4,18 @@ import Source from "../Source/Source";
 
 const PressConference = (props) => {
     return (
-        <div>
+        <div id={props.game.id}>
             <div className={PressConferencesCss.match}>
-                <div>
-                    <h4>
-                        {props.game.tournament.name}, {props.game.tour} матч: {props.game.first_team.name}({props.game.first_team_score}) - {props.game.second_team.name}({props.game.second_team_score})
-                    </h4>
+                <div className={PressConferencesCss.result}>
+                    {props.game.tournament.name}, {props.game.tour} матч: {props.game.first_team.name}({props.game.first_team_score}) - {props.game.second_team.name}({props.game.second_team_score})
                 </div>
                 <Source source={props.game.video_source.source} type='video' />
                 <Source source={props.game.text_source.source} type='text' />
             </div>
-            <div>
-                <button>
-                    Комментировать
-                </button>
-                <button>
-                    Смотреть комментарии
-                </button>
-            </div>
             <div dangerouslySetInnerHTML={{ __html: props.press_conference }} />
             <div>
                 <button>
-                    Комментировать
-                </button>
-                <button>
-                    Смотреть комментарии
+                    Комментировать / Смотреть комментарии
                 </button>
             </div>
             <hr/>
